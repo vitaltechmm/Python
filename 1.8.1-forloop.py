5e1c36b926f8d6824e010043a9889ac970e117b9
dream <dream3598@outlook.com>
2023-01-29 14:56:23 +0630

    for loop file update

---
+++ 1.8.1-forloop.py
@@ -0,0 +1,7 @@
+#for i in range (10):
+    #print(i)
+#input ကို integer input နဲ့မှရမယ်။
+first_num = int(input("Enter First Number :"))
+last_num = int(input("Enter Last Number :"))
+for i in range (first_num, last_num):
+    print(i)
